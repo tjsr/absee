d8609b0fe9ce6ef71e74f01e69e539c9a5319dd8
Tim Rowe <tim@tjsr.id.au>
2023-03-21 18:38:56 +1100

    Array length should be 1 when checking snowflake.

--- src/validate.ts
+++ src/validate.ts
@@ -3,5 +3,5 @@ export const isSnowflake = (data: string): boolean => {
     return false;
   }
   const regexMatch: RegExpMatchArray|null = data.match(/^\d{10,19}$/);
-  return regexMatch?.length == 0;
+  return regexMatch?.length == 1;
 }
