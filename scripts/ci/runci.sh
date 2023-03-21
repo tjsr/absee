b5f6bba8a76c5b7e85489583ce1366df3cd31e7f
Tim Rowe <tim@tjsr.id.au>
2023-03-21 19:26:53 +1100

    Need to pass REGISTRY_HOST all the way through.

--- scripts/ci/runci.sh
+++ scripts/ci/runci.sh
@@ -12,6 +12,6 @@ CONTAINER_ID=$(docker run --privileged --rm --env-file=.env -d -t tmp)
 echo Pausing 10 seconds to allow container to start
 sleep 10
 
-docker exec -e BITBUCKET_REPO_SLUG=$BITBUCKET_REPO_SLUG -e BITBUCKET_COMMIT=$BITBUCKET_COMMIT -t $CONTAINER_ID /bin/sh scripts/containers/build-containers.sh
-docker exec -e BITBUCKET_REPO_SLUG=$BITBUCKET_REPO_SLUG -e BITBUCKET_COMMIT=$BITBUCKET_COMMIT -t $CONTAINER_ID /bin/sh scripts/containers/push-containers.sh
+docker exec -e REGISTRY_HOST=$REGISTRY_HOST BITBUCKET_REPO_SLUG=$BITBUCKET_REPO_SLUG -e BITBUCKET_COMMIT=$BITBUCKET_COMMIT -t $CONTAINER_ID /bin/sh scripts/containers/build-containers.sh
+docker exec -e REGISTRY_HOST=$REGISTRY_HOST BITBUCKET_REPO_SLUG=$BITBUCKET_REPO_SLUG -e BITBUCKET_COMMIT=$BITBUCKET_COMMIT -t $CONTAINER_ID /bin/sh scripts/containers/push-containers.sh
 docker stop $CONTAINER_ID
