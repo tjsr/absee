bdc3de0b8e01af64354ccd0c6a2d21aafa1bbecc
Tim Rowe <tim@tjsr.id.au>
2023-09-25 20:49:14 +1000

    Make exception clearer.

--- src/pins/pinpanion.ts
+++ src/pins/pinpanion.ts
@@ -114,7 +114,7 @@ const getObjectForId = (sourceData: PinpanionData, id: string): Pin => {
   if (sourcePin) {
     return convertToDisplayPin(sourcePin);
   }
-  throw new Error(`Couldn't find pin for ${typeof id} id ${id}`);
+  throw new Error(`Couldn't find pin for (${typeof id}) id [${id}]`);
 };
 
 const datasourceConvertor = <PinpanionData>(inputData: any): PinpanionData => {
