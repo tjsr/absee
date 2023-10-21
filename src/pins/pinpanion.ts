127df1eadb091fbe308e79e3f5f11e22cfaa57b7
Tim Rowe <tim@tjsr.id.au>
2023-10-21 21:29:55 +1100

    Switch to three elements per comparison.

--- src/pins/pinpanion.ts
+++ src/pins/pinpanion.ts
@@ -132,5 +132,5 @@ export const loader: CollectionTypeLoader<Pin, PinpanionData> = {
   getObjectByIndex: getObjectForIndex,
   getObjectForId: getObjectForId,
   getObjectId: getObjectId,
-  maxElementsPerComparison: 1,
+  maxElementsPerComparison: 3,
 };
