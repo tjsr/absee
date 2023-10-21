f4484f73f9ce7b2a1d6ba7a1595000294d676a5e
Tim Rowe <tim@tjsr.id.au>
2023-10-21 21:53:22 +1100

    Allow element groups to be more easily determined.

--- src/ui/collection.styles.ts
+++ src/ui/collection.styles.ts
@@ -4,11 +4,12 @@ export const ComparisonElement = styled.div<{paddingLeft?: number, paddingRight?
 display: flex;
 flex-wrap: wrap;
 max-width: 36rem;
-/* border: 1px solid #000; */
+border: 1px solid #000;
 text-align: center;
-/* background-color: #e1e1e1; */
+background-color: #e1e1e1;
 align-items: center;
 justify-content: center;
+margin: 0.25rem;
 padding-left: ${({ paddingLeft }) => paddingLeft !== undefined ? paddingLeft : 0.5}rem;
 padding-right: ${({ paddingRight }) => paddingRight !== undefined ? paddingRight : 0.5}rem;
 `;
